4fe2166f73e19b8df8dc7e66f3786d0c51933cfe
Mohammad Sohel Khan <sohelkhan0378@gmail.com>
2020-11-25 19:46:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    $("#headerid").load("./components/header.html");
+});
+
+$(function() {
+    $("#footerid").load("./components/footer.html");
+});
+
+$(function(){
+    $("#navbarid").load("./components/navbar.html");
+});
